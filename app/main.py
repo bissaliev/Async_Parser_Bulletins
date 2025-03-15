e10bf47877415f869680ffb76cdd7b13ae99e663
bissaliev <bissaliev21@gmail.com>
2025-03-15 17:19:29 +0400

    Изменил модуль main.py: Добавил семафор для БД; Вынес функции запросов в отдельный модуль

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import asyncio
-import io
 import time
 from datetime import date, datetime
 
@@ -7,6 +6,7 @@ import aiohttp
 from database.crud import mass_create_trade
 from logging_config import logger
 from parsers.parser import Parser
+from parsers.scraper import fetch_file, fetch_page
 from utils.file_utils import XLSExtractor
 
 BASE_URL = "https://spimex.com"
@@ -14,58 +14,41 @@ PAGE_URL = BASE_URL + "/markets/oil_products/trades/results/"
 CURRENT_YEAR = datetime.now().year
 MIN_YEAR = 2023
 FIRST_PAGE = 1
-LAST_PAGE = 384
+LAST_PAGE = 55
 MAX_CONCURRENT_REQUESTS = 15  # Максимальное число одновременных запросов
+MAX_DB_CONCURRENT = 10  # Ограничение для операций с базой данных
 
 
-async def fetch_file(session: aiohttp.ClientSession, url: str):
-    """Скачиваем файл и отдаем контент"""
-    try:
-        async with session.get(url, raise_for_status=True) as response:
-            return await response.read()
-    except aiohttp.ClientResponseError as e:
-        logger.error(f"Ошибка при скачивание страницы файла: {e}")
-    except aiohttp.ClientError as e:
-        logger.error(f"Ошибка при скачивание файла: {e}")
-
-
-async def download_data(session: aiohttp.ClientSession, url: str, bidding_date: date):
+async def download_data(
+    session: aiohttp.ClientSession, url: str, bidding_date: date, semaphore: asyncio.Semaphore
+) -> None:
     """Скачивает файл, обрабатывает его и сохраняет данные в БД"""
 
     content = await fetch_file(session, url)
 
-    # Создаем класс извлекающий нужные данные из файла xls
-    xls_extractor = XLSExtractor(io.BytesIO(content), bidding_date)
-    data = xls_extractor.get_data()
+    if content is None:
+        logger.error(f"Не удалось скачать файл с торгами за {bidding_date} период")
+    else:
+        # Создаем класс извлекающий нужные данные из файла xls
+        xls_extractor = XLSExtractor(content, bidding_date)
+        data = xls_extractor.get_data()
 
-    # Сохраняем данные в БД
-    await mass_create_trade(data)
-
-
-async def fetch(session: aiohttp.ClientSession, url: str, params=None):
-    """Запрашиваем страницу и отдаем html-страницу"""
-    try:
-        async with session.get(url, params=params) as response:
-            response.raise_for_status()
-            logger.info(f"Страница {response.url} загружена")
-            return await response.text()
-    except aiohttp.ClientResponseError as e:
-        logger.error(f"Ошибка при скачивание страницы {params['page']}: {e.status}")
-    except aiohttp.ClientError as e:
-        logger.error(f"Ошибка при скачивание страницы {params['page']}: {e}")
+        # Сохраняем данные в БД
+        async with semaphore:
+            await mass_create_trade(data)
 
 
-async def process_page(session: aiohttp.ClientSession, page: int):
+async def process_page(session: aiohttp.ClientSession, page: int, semaphore: asyncio.Semaphore):
     """Обрабатывает одну страницу: парсит ссылки и загружает файлы"""
-    html = await fetch(session, PAGE_URL, params={"page": f"page-{page}"})
-    if html is None:
+    page_html = await fetch_page(session, PAGE_URL, params={"page": f"page-{page}"})
+    if page_html is None:
         logger.warning(f"Пропускаем страницу {page}, так как HTML не был загружен")
         return
-    parser = Parser(html, MIN_YEAR, CURRENT_YEAR)
-    file_links = parser.extract_file_links()
+    parser = Parser(page_html, MIN_YEAR, CURRENT_YEAR)
+    file_links: list[tuple[str, date]] = parser.extract_file_links()
     tasks = []
     for link, bidding_date in file_links:
-        tasks.append(asyncio.create_task(download_data(session, BASE_URL + link, bidding_date)))
+        tasks.append(asyncio.create_task(download_data(session, BASE_URL + link, bidding_date, semaphore)))
     await asyncio.gather(*tasks)
     logger.info(f"Страница {page} загружена")
 
@@ -74,10 +57,11 @@ async def main():
     """Главный модуль"""
     # В цикле проходимся по страницам со ссылка на файлы
     tasks = []
+    semaphore_db = asyncio.Semaphore(MAX_DB_CONCURRENT)
     connector = aiohttp.TCPConnector(limit=MAX_CONCURRENT_REQUESTS)
     async with aiohttp.ClientSession(connector=connector) as session:
         for page in range(FIRST_PAGE, LAST_PAGE + 1):
-            tasks.append(asyncio.create_task(process_page(session, page)))
+            tasks.append(asyncio.create_task(process_page(session, page, semaphore_db)))
         try:
             await asyncio.gather(*tasks)
             logger.info("Загрузка завершена")
@@ -90,10 +74,3 @@ if __name__ == "__main__":
     asyncio.run(main())
     end_time = time.perf_counter()
     print(f"Время выполнения: {end_time - start_time}")
-
-
-# Время выполнения (5 страниц) 11.814728350000223
-
-# Время выполнения с использованием to_thread (5 страниц): 10.282230708999123
-# Время выполнения с вложенными тасками (5 страниц): 9.705754711998452
-# Время выполнения с вложенными тасками (30 страниц): 27.03962082599901
