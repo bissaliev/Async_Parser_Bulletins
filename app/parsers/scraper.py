937b0e137ae22fe6676e0cb8528e787d7de3bbf5
bissaliev <bissaliev21@gmail.com>
2025-03-15 17:16:10 +0400

    Перенес функции для запросов в отдельный модуль app/parser/scraper.py

--- app/parsers/scraper.py
+++ app/parsers/scraper.py
@@ -1,46 +1,27 @@
 import io
 
-import requests
-from exceptions import RequestProcessingError
+import aiohttp
 from logging_config import logger
-from requests.exceptions import ConnectionError, RequestException, Timeout
 
 
-def get_request(url: str) -> requests.Response:
-    """Выполняет GET-запрос по указанному URL и возвращает объект Response"""
+async def fetch_page(session: aiohttp.ClientSession, url: str, params=None) -> str | None:
+    """Запрашиваем страницу и отдаем html-страницу"""
     try:
-        response = requests.get(url, timeout=10)
-        response.raise_for_status()
-        return response
-    except (ConnectionError, Timeout) as e:
-        raise RequestProcessingError(
-            f"Ошибка при подключении или тайм-аут для страницы {url}: {e}"
-        ) from e
-    except RequestException as e:
-        raise RequestProcessingError(
-            f"Ошибка запроса при получении страницы {url}: {e}"
-        ) from e
-    except Exception as e:
-        raise RequestProcessingError(
-            f"Неизвестная ошибка при получении страницы {url}: {e}"
-        ) from e
+        async with session.get(url, params=params) as response:
+            response.raise_for_status()
+            logger.info(f"Страница {response.url} загружена")
+            return await response.text()
+    except aiohttp.ClientResponseError as e:
+        logger.error(f"Ошибка при получении страницы {params['page']}: {e.status}")
+        return None
 
 
-def get_page(url: str) -> str:
-    """Получает HTML-код страницы по указанному URL"""
+async def fetch_file(session: aiohttp.ClientSession, url: str) -> io.BytesIO | None:
+    """Скачиваем файл и отдаем контент байтов"""
     try:
-        response = get_request(url)
-        return response.text
-    except RequestProcessingError as e:
-        logger.error(str(e))
-        raise RequestProcessingError(e) from e
-
-
-def download_file_as_bytes(url: str) -> io.BytesIO:
-    """Скачивает файл по ссылке и возвращает его в виде потока байтов"""
-    try:
-        response = get_request(url)
-        return io.BytesIO(response.content)
-    except RequestProcessingError as e:
-        logger.error(str(e))
-        raise RequestProcessingError(e) from e
+        async with session.get(url, raise_for_status=True) as response:
+            response.raise_for_status()
+            return io.BytesIO(await response.read())
+    except aiohttp.ClientResponseError as e:
+        logger.error(f"Ошибка при скачивание страницы файла: {e}")
+        return None
