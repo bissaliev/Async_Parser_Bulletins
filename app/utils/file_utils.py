f76de20453842a3fb5b367de0a51a83d99c71f3a
bissaliev <bissaliev21@gmail.com>
2025-03-16 11:49:27 +0400

    Добавил обработку исключений в случае если файл пустой или не того формата в класс app.utils.file_utils.XLSExtractor

--- app/utils/file_utils.py
+++ app/utils/file_utils.py
@@ -15,9 +15,15 @@ class XLSExtractor:
     table_name = "Единица измерения: Метрическая тонна"
 
     def __init__(self, file: io.BytesIO, bidding_date: date):
-        self.bidding_date = bidding_date
-        self.dataframe: pd.DataFrame = self._load_xls(file)
-        logger.info(f"Файл загружен для даты {bidding_date}")
+        try:
+            if file.getbuffer().nbytes == 0:
+                raise ValueError("Файл пустой, загрузка невозможна!")
+            else:
+                self.bidding_date = bidding_date
+                self.dataframe: pd.DataFrame = self._load_xls(file)
+                logger.info(f"Файл преобразован в DataFrame для даты {bidding_date}")
+        except ValueError as e:
+            raise XLSExtractorError(e) from e
 
     def _load_xls(self, file) -> pd.DataFrame:
         """Загружает данные из xls-файла в DataFrame."""
@@ -25,9 +31,7 @@ class XLSExtractor:
 
     def _find_start_index(self) -> int:
         """Находит индекс строки, содержащей ключевую фразу."""
-        match = (
-            self.dataframe.stack().astype(str).str.contains(self.table_name)
-        )
+        match = self.dataframe.stack().astype(str).str.contains(self.table_name)
         indices = match[match].index.get_level_values(0).unique()
 
         if indices.empty:
@@ -38,18 +42,14 @@ class XLSExtractor:
         """Обрезает DataFrame с нужного индекса и переименовывает колонки."""
         start_idx = self._find_start_index()
         self.df = self.dataframe.iloc[start_idx + 1 :].reset_index(drop=True)
-        self.df.columns = (
-            self.df.iloc[0].astype(str).str.replace("\n", " ").str.strip()
-        )
+        self.df.columns = self.df.iloc[0].astype(str).str.replace("\n", " ").str.strip()
         return self.df.iloc[1:].reset_index(drop=True)  # Убираем заголовок
 
     def _filter_valid_rows(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         Фильтрует строки, оставляя только те, где количество договоров > 0.
         """
-        count_col = next(
-            col for col in df.columns if "Количество Договоров" in col
-        )
+        count_col = next(col for col in df.columns if "Количество Договоров" in col)
         df = df[df[count_col].astype(str).str.isnumeric()]
         df = df[df[count_col].astype(int) > 0]
         return df.iloc[:-2]  # Удаляем последние 2 строки с итогами
@@ -80,13 +80,9 @@ class XLSExtractor:
     def get_data(self) -> list[dict[str, Any]]:
         """Возвращает данные в виде списка словарей"""
         try:
-            data = self._to_dict(
-                self._filter_valid_rows(self._extract_table())
-            )
-            logger.info(f"Данные успешно обработаны для {self.bidding_date}")
+            data = self._to_dict(self._filter_valid_rows(self._extract_table()))
             return data
         except (ValueError, KeyError, StopIteration) as e:
-            logger.error(f"Ошибка обработки XLS-файла: {e}")
-            raise XLSExtractorError(
-                f"Ошибка при обработке XLS-файла: {e}"
-            ) from e
+            raise XLSExtractorError(f"Ошибка при обработке XLS-файла: {e}") from e
+        except Exception as e:
+            raise XLSExtractorError(f"Неизвестная ошибка при обработке файла: {e}") from e
